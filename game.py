7be2b7b29957064bb4478b16aa9cfbd035b6adc3
Maxim Bazant <maxim.bazant@outlook.com>
2020-07-05 17:59:59 +0200

    initial commit

--- game.py
+++ game.py
@@ -1,5 +1,26 @@
+#  tutorial 1 - basic pygame info
+
 import pygame
 
 pygame.init()
 
-print(5)
+win_width, win_height = 600, 400
+win = pygame.display.set_mode((win_width, win_height))
+win_caption = pygame.display.set_caption("space shooter game")
+
+running = True
+
+clock = pygame.time.Clock()
+
+FPS = 30
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(FPS)
+
+pygame.quit()
+quit()
+

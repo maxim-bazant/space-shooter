a38501c034132e9075c92d4fb5639203d2ce8223
Maxim Bazant <maxim.bazant@outlook.com>
2020-07-09 16:23:45 +0200

    simple space ship bliting and moving

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import pygame
 
 pygame.init()
 
-win_width, win_height = 600, 400
+win_width, win_height = 1000, 900
 win = pygame.display.set_mode((win_width, win_height))
 win_caption = pygame.display.set_caption("space shooter game")
 
@@ -14,11 +14,42 @@ clock = pygame.time.Clock()
 
 FPS = 30
 
+
+class SpaceShip(object):
+    def __init__(self):
+        self.image = pygame.image.load("images/space_ship.png").convert_alpha()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.x = 20
+        self.y = win_height - self.height - 50
+        self.rotation_angle = 0
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_d]:
+            self.x += 5
+            win.blit(self.image, (self.x, self.y))
+
+        elif keys[pygame.K_a]:
+            self.x -= 5
+            win.blit(self.image, (self.x, self.y))
+
+        else:
+            win.blit(self.image, (self.x, self.y))
+
+
+space_ship = SpaceShip()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    win.fill((0, 0, 0))
+    space_ship.move()
+
+    pygame.display.update()
     clock.tick(FPS)
 
 pygame.quit()
